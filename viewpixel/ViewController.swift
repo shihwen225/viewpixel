2be8495ad85b1c7eaa5df59671aa3be851538744
鄭小玟 <shihwen225@ymail.com>
2018-03-18 22:43:33 +0800

    Initial Commit

---
+++ viewpixel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  viewpixel
+//
+//  Created by shih wen Cheng on 2018/3/18.
+//  Copyright © 2018年 shih wen Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
